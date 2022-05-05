e7de46b6dd0e0f7e98c43e2a917f8a0c5cf5cfe4
bon_laptop <502285815@qq.com>
2022-05-05 23:23:08 +0800

    pretty blog

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -2,8 +2,6 @@ import {navbar} from "vuepress-theme-hope";
 
 export default navbar([
     "/",
-    "/home",
-    {text: "使用指南", icon: "creative", link: "/guide/"},
     {
         text: "博文",
         icon: "edit",
@@ -18,17 +16,6 @@ export default navbar([
                     "install_giscus"
                 ],
             }, {
-                text: "虎彩工作日志",
-                prefix: "hucai_work/",
-                children: [
-                    "03_month",
-                    "04_month",
-                    "05_month",
-                    "work_info",
-                    "devlop_log",
-                    "tenant_develop_extend",
-                ]
-            }, {
                 text: "git 文章",
                 prefix: "git/",
                 children: [
@@ -36,10 +23,16 @@ export default navbar([
                 ]
             }
         ],
-    },
-    {
-        text: "主题文档",
-        icon: "note",
-        link: "https://vuepress-theme-hope.github.io/v2/zh/",
-    },
+    }, {
+        text: "工作",
+        prefix: "/hucai_work/",
+        children: [
+            "03_month",
+            "04_month",
+            "05_month",
+            "work_info",
+            "devlop_log",
+            "tenant_develop_extend",
+        ]
+    }
 ]);
